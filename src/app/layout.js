5970a8bcd7c76bf4dff3f909fea54bbb688e7647
Samarajya Shrestha <samarajyastha@gmail.com>
2025-04-27 21:28:14 +0545

    feat: init next.js app

---
+++ src/app/layout.js
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "E-Bazaar",
+  description: "Ecommerce app on Next.js",
+  keywords: "E-commerce, online shopping",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
