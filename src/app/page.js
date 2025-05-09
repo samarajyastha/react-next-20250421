68aa1c1fb2569b5a693b79023d4f43e00afcab2a
Samarajya Shrestha <samarajyastha@gmail.com>
2025-05-09 21:13:24 +0545

    feat: tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div>Home</div>;
+  return <div className="text-xl sm:text-3xl md:text-4xl lg:text-6xl xl:text-9xl xl:text-amber-300">Home</div>;
 };
 
 export default Home;
